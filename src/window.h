2ecae1fc6976cda25e980d81eb30d1581c0184d8
Brandon Li <brandonli2006ma@gmail.com>
2026-01-29 12:28:53 -0800

    Added License and License Headers

--- src/window.h
+++ src/window.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Pulse Media Player, a media player written in C++
+ * Copyright (C) Brandon Li <https://brandonli.me/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <functional>
@@ -7,9 +25,10 @@ constexpr int DEFAULT_WIDTH = 1920;
 constexpr int DEFAULT_HEIGHT = 1080;
 constexpr auto DEFAULT_WINDOW_TITLE = "Pulse Media Player";
 
-GLFWwindow* getWindow();
+GLFWwindow *getWindow();
 
 void init_gl_window();
+
 void destroy_gl_window();
 
 inline bool should_stop() {
